b9ba1ba81d6ff6d6e2981c20d654358714fc137a
rueangwit suporn <ruengwit.cs54@ubru.ac.th>
2017-09-29 17:02:24 +0700

    d

--- schoolnew/app/Http/Controllers/SocialLoginController.php
+++ schoolnew/app/Http/Controllers/SocialLoginController.php
@@ -15,10 +15,12 @@ class SocialLoginController extends Controller
     return Socialize::with('facebook')->redirect();
   }
   public function facebookSuccess() {
-    $provider = Socialize::driver('facebook');
+    $provider = Socialize::driver('facebook')->user();
+    //\Socialite::driver('facebook')->user();
     //$user = Socialite::driver('github')->user();
 
       dd($provider); // print value debug.
+      echo $provider->name;
 
   }
 }
