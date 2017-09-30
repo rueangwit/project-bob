7c97391230fb3b04e0551a6b61b6d50ecc5d3df3
rueangwit suporn <ruengwit.cs54@ubru.ac.th>
2017-09-30 15:19:26 +0700

    30092560

--- schoolnew/app/Http/Controllers/SocialLoginController.php
+++ schoolnew/app/Http/Controllers/SocialLoginController.php
@@ -2,25 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\SocialAccountService;
+
 
 use Socialize;
-use Input;
+
 use Auth;
 
 class SocialLoginController extends Controller
 {
   public function facebookAuthRedirect() {
-    return Socialize::with('facebook')->redirect();
+    return Socialize::driver('facebook')->redirect();
   }
-  public function facebookSuccess() {
-    $provider = Socialize::driver('facebook')->user();
+  public function facebookSuccess(SocialAccountService $service) {
+    //$provider = Socialize::driver('facebook')->user();
     //\Socialite::driver('facebook')->user();
     //$user = Socialite::driver('github')->user();
 
       //'dd($provider); // print value debug.
-      echo $provider->name;
+      //echo $provider->name."ชื่อ: ".$provider->email;
+
+      $user = $service->createOrGetUser(Socialize::driver('facebook')->user());
+
+        auth()->login($user);
+
+        return redirect()->to('/home');
 
   }
 }
--- schoolnew/resources/views/layouts/app.blade.php
+++ schoolnew/resources/views/layouts/app.blade.php
@@ -14,6 +14,30 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+  <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '130747934239429',
+      xfbml      : true,
+      version    : 'v2.10'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
